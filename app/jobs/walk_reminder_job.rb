0a4bfc001e3895d77c59d770a679c6ad5c336cfd
Brigham <brig.johnson@gmail.com>
2024-05-21 14:46:32 -0400

    set up 'perform' for dog-based 'walk_reminder' job to print a signal if dog needs a walk

--- app/jobs/walk_reminder_job.rb
+++ app/jobs/walk_reminder_job.rb
@@ -1,7 +1,10 @@
 class WalkReminderJob < ApplicationJob
   queue_as :default
 
-  def perform(*args)
-    # Do something later
+  def perform(dog)
+    if dog.last_walked_at < 8.hours.ago
+      puts "Reminder! Time to walk: #{dog.name}"
+      # Here you could also send an email or notification
+    end
   end
 end
